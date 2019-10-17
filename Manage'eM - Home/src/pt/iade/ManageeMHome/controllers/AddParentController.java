4c09c5945d6431a549ea6fccc12facd84524bb84
Jaime Ferreira <jaimeferreira2000@gmail.com>
2019-10-17 14:29:19 +0100

    ATTEMPT OF UNDOING THIS BIG MESS UP

---
+++ Manage'eM - Home/src/pt/iade/ManageeMHome/controllers/AddParentController.java
@@ -0,0 +1,13 @@
+package pt.iade.ManageeMHome.controllers;
+
+import javafx.fxml.FXML;
+import pt.iade.ManageeMHome.Main;
+
+public class AddParentController {
+
+	@FXML
+	public void addButtonOnCLick() {
+
+		Main.parentPlusStage.close();
+	}
+}
---
+++ Manage'eM - Home/src/pt/iade/ManageeMHome/controllers/AddRewardController.java
@@ -0,0 +1,13 @@
+package pt.iade.ManageeMHome.controllers;
+
+import javafx.fxml.FXML;
+import pt.iade.ManageeMHome.Main;
+
+public class AddRewardController {
+
+	@FXML
+	public void addButtonOnCLick() {
+
+		Main.rewardPlusStage.close();
+	}
+}
